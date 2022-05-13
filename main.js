0d903b32dc13a4e8b9e49963cdc8d4f081329c9f
Brian Mayo <ioprotium@gmail.com>
2022-05-13 16:11:17 +0200

    remove meed

--- main.js
+++
@@ -1,9 +0,0 @@
-const MEDIUM_FEED_URL = "https://medium.com/feed/@protiumx";
-
-async function fecthLastMediumArticles() {
-  const feed = new Meed({});
-  const user = await feed.user("protiumx");
-  console.log(user);
-}
-
-fecthLastMediumArticles();

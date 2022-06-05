5e849606c8f8c0040090672f5b8ac0ef0f87bbd6
Brian Mayo <ioprotium@gmail.com>
2022-06-05 23:59:11 +0200

    ci: use deno script to fetch last blog posts

---
+++ .github/workflows/rss.ts
@@ -0,0 +1,12 @@
+import { parse } from "https://deno.land/x/xml/mod.ts";
+
+const res = await fetch('https://protiumx.dev/blog/index.xml');
+const rss = parse(await res.text()).rss! as any;
+
+const output = [];
+for (const {title, link} of rss.channel.item) {
+  output.push(`# ${title}\r\n${link}\r\n`);
+}
+
+console.log(output.join('\n'));
+

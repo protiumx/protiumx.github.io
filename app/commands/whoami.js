1a58a117e2f49247d72449cc905ce0179f7d6615
Brian Mayo <ioprotium@gmail.com>
2022-06-06 03:54:29 +0200

    refator: remove host

--- app/commands/whoami.js
+++ app/commands/whoami.js
@@ -1,7 +1,7 @@
 import {TermColors} from "../constants.js";
 import {colorize} from "../utils.js";
 
-const LAST_UPDATE = '2022-05-26';
+const LAST_UPDATE = '2022-06-06';
 
 const whoami = {
   id: "whoami",
@@ -9,8 +9,7 @@ const whoami = {
   description: 'display effective developer info',
   async exec(term, _args) {
     term.writeln(colorize(TermColors.Green, 'name: ') + 'brian');
-    term.writeln(colorize(TermColors.Green, 'host: ') + window.location.host);
-    term.writeln(colorize(TermColors.Green, 'current position: ') + 'software engineer');
+    term.writeln(colorize(TermColors.Green, 'current position: ') + 'senior software engineer');
     term.writeln(colorize(TermColors.Green, 'company: ') + 'Heartbeat Medical Solutions < https://heartbeat-med.de >');
     term.writeln(colorize(TermColors.Green, 'location: ') + 'Munich, Germany');
     term.writeln(colorize(TermColors.Green, 'fav languages: ') + '[golang, rust, typescript]');

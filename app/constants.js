58ac3b460d8d4ae032eb5fa4ed53cf057aa243c7
Brian Mayo <ioprotium@gmail.com>
2022-11-16 10:54:41 +0100

    refactor: move history size as const

--- app/constants.js
+++ app/constants.js
@@ -1,10 +1,13 @@
 // Octal codes are not allowed in strict mode.
 // Hence the hexadecimal escape sequence
 export const TermColors = {
-  Red: '\x1B[1;31m',
-  Green: '\x1B[1;32m',
-  Purple: '\x1B[1;35m',
-  Reset: '\x1B[0m',
+  Red: "\x1B[1;31m",
+  Green: "\x1B[1;32m",
+  Purple: "\x1B[1;35m",
+  Reset: "\x1B[0m",
 };
 
-export const SHELL_PROMPT = TermColors.Purple + 'anonymous@protiumx.dev:~$ ' + TermColors.Reset;
+export const SHELL_PROMPT =
+  TermColors.Purple + "anonymous@protiumx.dev:~$ " + TermColors.Reset;
+
+export const HistorySize = 100;

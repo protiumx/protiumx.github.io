1f175426f99f44f0d53d88526356e35cd4d07c48
Brian Mayo <ioprotium@gmail.com>
2022-11-17 14:43:29 +0100

    refactor: update versioning

--- app/commands/whoami.js
+++ app/commands/whoami.js
@@ -1,21 +1,35 @@
-import {TermColors} from "../constants.js";
-import {colorize} from "../utils.js";
+import { TermColors } from "../constants.js";
+import { colorize } from "../utils.js";
 
-const LAST_UPDATE = '2022-06-06';
+const LAST_UPDATE = "2022-11-17";
 
 const whoami = {
   id: "whoami",
   args: 0,
-  description: 'display effective developer info',
+  description: "display effective developer info",
   async exec(term, _args) {
-    term.writeln(colorize(TermColors.Green, 'name: ') + 'brian');
-    term.writeln(colorize(TermColors.Green, 'current position: ') + 'senior software engineer');
-    term.writeln(colorize(TermColors.Green, 'company: ') + 'Heartbeat Medical Solutions < https://heartbeat-med.de >');
-    term.writeln(colorize(TermColors.Green, 'location: ') + 'Munich, Germany');
-    term.writeln(colorize(TermColors.Green, 'fav languages: ') + '[golang, rust, typescript]');
-    term.writeln(colorize(TermColors.Green, 'hobbies: ') + '[photography, music, electronics]');
-    term.writeln(colorize(TermColors.Green, 'blog: ') + 'https://protiumx.dev/blog');
-    term.writeln(colorize(TermColors.Green, 'last update: ') + LAST_UPDATE);
+    term.writeln(colorize(TermColors.Green, "name: ") + "brian");
+    term.writeln(
+      colorize(TermColors.Green, "current position: ") +
+        "senior software engineer"
+    );
+    term.writeln(
+      colorize(TermColors.Green, "company: ") +
+        "Heartbeat Medical Solutions < https://heartbeat-med.de >"
+    );
+    term.writeln(colorize(TermColors.Green, "location: ") + "Munich, Germany");
+    term.writeln(
+      colorize(TermColors.Green, "fav languages: ") +
+        "[golang, rust, typescript]"
+    );
+    term.writeln(
+      colorize(TermColors.Green, "hobbies: ") +
+        "[photography, music, electronics]"
+    );
+    term.writeln(
+      colorize(TermColors.Green, "blog: ") + "https://protiumx.dev/blog"
+    );
+    term.writeln(colorize(TermColors.Green, "last update: ") + LAST_UPDATE);
   },
 };
 

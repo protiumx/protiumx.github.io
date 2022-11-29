92f663a455cd62eb3b1e0c58d31e42ef2afde85e
Brian Mayo <ioprotium@gmail.com>
2022-11-29 18:24:54 +0100

    refactor: format

--- app/commands/cat.js
+++ app/commands/cat.js
@@ -1,33 +1,38 @@
-import {TermColors} from '../constants.js';
-import fileSystem from '../file-system.js';
-import {colorize} from '../utils.js';
+import { TermColors } from "../constants.js";
+import fileSystem from "../file-system.js";
+import { colorize } from "../utils.js";
 
 const cat = {
   id: "cat",
-  description: 'print files',
-  usage: 'cat [file ...]',
+  description: "print files",
+  usage: "cat [file ...]",
   args: -1,
   async exec(term, args) {
     for (const fileName of args) {
       const file = fileSystem.get(fileName);
       if (!file) {
-        term.writeln(colorize(TermColors.Red, '[error]: ') + `"${fileName}": No such a file or directory`);
-        continue;
-      } 
-      if (!file.name.includes('.md')) {
-        term.writeln(colorize(TermColors.Red, '[error]: ') + `"${file.name}": file encoding not supported`);
+        term.write(
+          colorize(TermColors.Red, "[error]: ") +
+            `"${fileName}": No such a file`
+        );
         continue;
       }
-      for (const line of file.content.split('\n')) {
-        if (line.startsWith('#')) {
+      if (!file.name.includes(".md")) {
+        term.write(
+          colorize(TermColors.Red, "[error]: ") +
+            `"${file.name}": file encoding not supported`
+        );
+        continue;
+      }
+      for (const line of file.content.split("\n")) {
+        if (line.startsWith("#")) {
           term.writeln(colorize(TermColors.Green, line));
         } else {
           term.writeln(line);
         }
       }
     }
-  }
+  },
 };
 
 export default cat;
-
